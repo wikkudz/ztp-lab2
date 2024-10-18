94c8121768e873d2c692eed9dab9ec61ee093c1d
wikkudz <wikkudz@gmail.com>
2024-10-18 21:51:56 +0200

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'ztp-lab2';
+  text: string = '';
+
+
+}
